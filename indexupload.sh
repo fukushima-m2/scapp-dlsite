7e0f95068ff99e12fc71c718f9b08e5fc941c7b5
fukushima <58962158+fukushima-m2@users.noreply.github.com>
2020-12-18 15:37:25 +0900

    Update shell script

--- indexupload.sh
+++ indexupload.sh
@@ -7,3 +7,9 @@ gsutil cp ${FILE_NAME} gs://${GCS_BACKET_DIR}
 
 # set Cache-control
 gsutil setmeta -h 'Cache-Control:public, max-age=5' gs://${GCS_BACKET_DIR}/${FILE_NAME}
+
+echo;   # 改行
+ESC=$(printf '\033')
+# 青色でファイル名を表示（34 = BLUE）
+printf "Uploaded URL: ${ESC}[34m%s${ESC}[m\n" "https://storage.googleapis.com/${GCS_BACKET_DIR}/${FILE_NAME}"
+echo;   # 改行
--- upload.sh
+++ upload.sh
@@ -1,2 +1,13 @@
 sh indexupload.sh
 sh plistupload.sh
+
+export FILE_NAME=index.html
+export GCS_BACKET_DIR=stay-app-android
+export GOOGLE_PROJECT_ID=happyhotel-app-android
+
+echo;   # 改行
+echo Please upload IPA and APK:
+ESC=$(printf '\033')
+# URLを下線付きで表示（4 = 下線）
+printf "${ESC}[4m%s${ESC}[m\n" "https://console.cloud.google.com/storage/browser/${GCS_BACKET_DIR}"
+echo;   # 改行
